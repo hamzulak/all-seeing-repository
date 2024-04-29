bab95a901d46b1461e0a74a44a6fa8f467b555ba
hamzulak <148567107+hamzulak@users.noreply.github.com>
2024-04-29 14:52:20 +0200

    Made a grammatical mistake oopsie daisies

--- Dices&Programs.cpp
+++ Dices&Programs.cpp
@@ -14,7 +14,7 @@ int main() {
 
     int choice;
 
-    cout << "Welcome to the Dices & Programs!" << endl;
+    cout << "Welcome to Dices & Programs!" << endl;
 
     while (true) {
         cout << "\nChoose a dice to throw (from 2 to 20 sides), or enter 0 to exit: ";
